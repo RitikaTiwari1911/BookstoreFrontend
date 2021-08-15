947326d68557533222730a56efadb14413e46ddb
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-08-15 18:52:19 +0530

    refactor: added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Login from './pages/Login';
 import "../src/App.css"
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+require('dotenv').config()
 const App = ()=>{
   return(
     <>
-      <Login/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Login}/>
+      </Switch>
+    </BrowserRouter>
     </>  
   )
 }
--- src/service/user.js
+++ src/service/user.js
@@ -7,8 +7,9 @@
 -----------------------------------------------------------------------------------------------*/
 import Axios from 'axios'
 require('dotenv').config()
-//Axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
+//Axios.defaults.baseURL = 
 const BASE_URL = "http://localhost:4000";
+console.log('url',BASE_URL)
 export class User{
     userLogin = (loginDetails) =>{
         return Axios.post(BASE_URL+'/admin-login', loginDetails)
